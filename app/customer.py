68853afd58d6e96800dd4862b8a8c4193cf4ea4b
Aleksandr Mazur <a9p9m9@gmail.com>
2024-11-25 21:13:47 +0200

    Solutio

---
+++ app/customer.py
@@ -0,0 +1,71 @@
+import math
+
+from app.shop import Shop
+
+
+class Customer:
+    def __init__(
+            self,
+            name: str,
+            product_cart: dict,
+            location: list,
+            money: int,
+            fuel_consumption: float
+    ) -> None:
+
+        self.name = name
+        self.product_cart = product_cart
+        self.location = location
+        self.money = money
+        self.fuel_consumption = fuel_consumption
+
+    def print_customer_name_and_money(self) -> None:
+        print(f"{self.name} has {self.money} dollars")
+
+    def calculate_buyer_balance_of_money(self, money: int) -> None:
+        print(f"{self.name} now has {self.money - money} dollars\n")
+
+    def get_price_product_cart(self, shop: Shop, file_config: dict) -> int:
+        distance = (
+            math.sqrt(math.pow((self.location[0] - shop.location[0]), 2)
+                      + math.pow((self.location[1] - shop.location[1]), 2))
+        )
+
+        price_of_all_products = sum(
+            [value * shop.products[key]
+                for key, value in self.product_cart.items()
+                if key in shop.products]
+        )
+
+        price_fuel = (
+            (distance * 2 / 100 * self.fuel_consumption)
+            * file_config["FUEL_PRICE"]
+        )
+
+        print(
+            f"{self.name}'s trip to the {shop.name} costs "
+            f"{round(sum([price_of_all_products, price_fuel]), 2)}"
+        )
+
+        return round(sum([price_of_all_products, price_fuel]), 2)
+
+    def change_location_buyer(self, shop: Shop) -> None:
+        self.location = shop.location
+        print(f"{self.name} rides to {shop.name}\n")
+
+    def print_check(self, shop: Shop) -> None:
+        print("Date: 04/01/2021 12:33:41")
+        print(f"Thanks, {self.name}, for your purchase!")
+        print("You have bought:")
+
+        total_coast = 0.0
+        for key, value in self.product_cart.items():
+            total_coast += value * shop.products[key]
+            print(
+                f"{value} {key}s for "
+                f"{f'{value * shop.products[key]}'.rstrip('0').rstrip('.')} "
+                f"dollars"
+            )
+        print(f"Total cost is {total_coast} dollars")
+        print("See you again!\n")
+        print(f"{self.name} rides home")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,38 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as file:
+        file_config = json.load(file)
+
+    for customer in file_config["customers"]:
+        buyer = Customer(
+            customer["name"],
+            customer["product_cart"],
+            customer["location"],
+            customer["money"],
+            customer["car"]["fuel_consumption"]
+        )
+
+        buyer.print_customer_name_and_money()
+        price_in_shops = {}
+        for shop in file_config["shops"]:
+            shop = Shop(shop["name"], shop["location"], shop["products"])
+            price = buyer.get_price_product_cart(shop, file_config)
+            price_in_shops[shop] = price
+
+        shop_with_min_price = min(price_in_shops, key=price_in_shops.get)
+        min_price = price_in_shops[shop_with_min_price]
+
+        if buyer.money < min_price:
+            print(
+                f"{buyer.name} doesn't have enough money "
+                f"to make a purchase in any shop"
+            )
+        else:
+            buyer.change_location_buyer(shop_with_min_price)
+            buyer.print_check(shop_with_min_price)
+            buyer.calculate_buyer_balance_of_money(min_price)
